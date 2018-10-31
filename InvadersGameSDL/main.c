6fd251ab2403b8e792ec4393bff502f914065891
Marek <e8909@alunos.uminho.pt>
2018-10-31 14:09:53 +0000

    added SDL_TTF library added menus updated alien creation to spawn enough of them fixed aliens sometimes not dying (having negative health)
    TODO: proper cleanups
    TODO: reinitialization when game is lost or won and replay is clicked

--- InvadersGameSDL/main.c
+++ InvadersGameSDL/main.c
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_TTF.h>
 #include <SDL_image.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,7 +11,7 @@
 #define BULLET_SIZE_WIDTH 50
 #define BULLET_SIZE_HEIGHT 20
 
-#define TIME_STEP 1000/10
+#define TIME_STEP 1000/30
 #define DELAY 2
 
 #define GAME_STATE_MENU 1
@@ -18,26 +19,37 @@
 #define GAME_STATE_PAUSE 3
 #define GAME_STATE_QUIT 0
 
+#define MENU_STATE_WELCOME 0
+#define MENU_STATE_GAME_OVER 1
+#define MENU_STATE_VICTORY 2
+
+#define MENU_SELECTED_PLAY 0
+#define MENU_SELECTED_QUIT 1
+
 #define ALIEN_DIRECTION_RIGHT 1
 #define ALIEN_DIRECTION_LEFT -1
 
 #define ALIEN_VERTICAL_SPEED 40
 #define ALIEN_HORIZONTAL_SPEED 8
-#define ALIEN_HORIZONTAL_COUNT 4
-#define ALIEN_VERTICAL_COUNT 1
+#define ALIEN_HORIZONTAL_COUNT 15
+#define ALIEN_VERTICAL_COUNT 3
 #define ALIEN_COUNT (ALIEN_HORIZONTAL_COUNT * ALIEN_VERTICAL_COUNT)
+
+#define ALIEN_FIRING_DELAY 8000
+#define ALIEN_HEALTH 20
 
 #define PLAYER_SPEED 10
 #define PLAYER_FIRING_DELAY 1000
+#define PLAYER_HEALTH 50
 
 #define BULLET_SPEED 15
 
 #define GET_RESOURCE_PATH(res_name) "resources/"res_name
 
 // Define background color in R, G, B
-// #define R 0
-// #define G 0
-// #define B 0
+#define R 0
+#define G 0
+#define B 0
 
 // Creation of structs
 typedef struct {
@@ -51,8 +63,13 @@ typedef struct {
 	int health;
 } Entity;
 
+typedef struct {
+	SDL_Texture* texture;
+	SDL_Rect* rect;
+} TextButton;
+
 // Support functions
-SDL_Window* init()
+SDL_Window* initializeSdl()
 {
 	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -66,6 +83,11 @@ SDL_Window* init()
 	if (!(IMG_Init(imgFlags) & imgFlags))
 	{
 		printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+		exit(-1);
+	}
+
+	if (TTF_Init() == -1) {
+		printf("SDL_TTF could not initialize! TTF_Init: %s\n", TTF_GetError());
 		exit(-1);
 	}
 
@@ -84,33 +106,41 @@ Entity* createAliens(SDL_Renderer* renderer, int *alienCount) {
 	Entity* aliens = (Entity*)malloc(*alienCount * sizeof(Entity));
 
 	// Create aliens
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < *alienCount; i++) {
 		aliens[i].object.imageRect = (SDL_Rect*)malloc(sizeof(SDL_Rect));
-		aliens[i].health = 50;
+		aliens[i].health = ALIEN_HEALTH;
 	}
 
-	// Set names of images to aliens
-	aliens[0].object.imageName = GET_RESOURCE_PATH("alien_easy.png");
-	aliens[1].object.imageName = GET_RESOURCE_PATH("alien_medium.png");
-	aliens[2].object.imageName = GET_RESOURCE_PATH("alien_hard.png");
-	aliens[3].object.imageName = GET_RESOURCE_PATH("alien_boss.png");
+	char imageNames[][30] = {
+		{GET_RESOURCE_PATH("alien_easy.png") },
+		{GET_RESOURCE_PATH("alien_medium.png") },
+		{GET_RESOURCE_PATH("alien_hard.png")},
+		{GET_RESOURCE_PATH("alien_boss.png")}
+	};
 
+
+	int rowOffset = 0;
 	// Set texture, position and size of aliens
-	int offsetEntity = 2;
-	for (int i = 0; i < *alienCount; i++) {
-		// Set texture of alien image
-		aliens[i].object.imageText = IMG_LoadTexture(renderer, aliens[i].object.imageName);
+	for (int row = 0; row < ALIEN_VERTICAL_COUNT; row++) {
+		int indexOffset = row * ALIEN_HORIZONTAL_COUNT;
+		int colOffset = 2;
+		rowOffset += (ENTITY_SIZE + 2);
+		for (int col = 0; col < ALIEN_HORIZONTAL_COUNT; col++) {
+			// Set texture of alien image
+			aliens[indexOffset + col].object.imageName = imageNames[row];
+			aliens[indexOffset + col].object.imageText = IMG_LoadTexture(renderer, aliens[indexOffset + col].object.imageName);
 
-		// Set postion of each alien
-		aliens[i].object.imageRect->x = offsetEntity;
-		aliens[i].object.imageRect->y = 0;
+			// Set postion of each alien
+			aliens[indexOffset + col].object.imageRect->x = colOffset;
+			aliens[indexOffset + col].object.imageRect->y = rowOffset;
 
-		// Set size of each alien
-		aliens[i].object.imageRect->w = ENTITY_SIZE;
-		aliens[i].object.imageRect->h = ENTITY_SIZE;
+			// Set size of each alien
+			aliens[indexOffset + col].object.imageRect->w = ENTITY_SIZE;
+			aliens[indexOffset + col].object.imageRect->h = ENTITY_SIZE;
 
-		// Make small space between aliens => + 2
-		offsetEntity += ENTITY_SIZE + 2;
+			// Make small space between aliens => + 2
+			colOffset += ENTITY_SIZE + 2;
+		}
 	}
 	return aliens;
 }
@@ -118,7 +148,7 @@ Entity* createAliens(SDL_Renderer* renderer, int *alienCount) {
 Entity* createShip(SDL_Renderer* renderer) {
 	// Create ship
 	Entity* ship = (Entity*)malloc(sizeof(Entity));
-	ship->health = 50;
+	ship->health = PLAYER_HEALTH;
 
 	// Set image name
 	ship->object.imageName = GET_RESOURCE_PATH("shipInvaders.png");
@@ -158,7 +188,6 @@ Object* createBullet(Entity* entity, SDL_Renderer* renderer, bool setAlienBullet
 
 	// Set image texture
 	bullet->imageText = IMG_LoadTexture(renderer, bullet->imageName);
-	char* a = SDL_GetError();
 
 	int x = entity[position].object.imageRect->x;
 	int y = entity[position].object.imageRect->y;
@@ -358,7 +387,7 @@ void evaluateCollisions(Object** bullets, int* bulletCount, Entity** entities, i
 				// TODO: Check if alien is already add to destAliens array
 				//       - should not be necessary, should be done for bullets though
 				(*entities)[j].health -= 10;
-				if ((*entities)[j].health == 0) {
+				if ((*entities)[j].health <= 0) {
 					destroyEntities = (Entity**)realloc(destroyEntities, (destroyEntitiesCount + 1) * sizeof(Entity*));
 					destroyEntities[destroyEntitiesCount] = &((*entities)[j]);
 					destroyEntitiesCount++;
@@ -386,11 +415,88 @@ void evaluateCollisions(Object** bullets, int* bulletCount, Entity** entities, i
 	}
 }
 
+TextButton createTextButton(SDL_Renderer* renderer, TTF_Font* font, SDL_Color color, char* text, int x, int y) {
+	SDL_Surface* textSurface = TTF_RenderText_Solid(font, text, color);
+	SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+	TextButton b;
+	b.texture = textTexture;
+	b.rect = (SDL_Rect*)malloc(sizeof(SDL_Rect));
+	b.rect->w = textSurface->w;
+	b.rect->h = textSurface->h;
+	b.rect->x = x - (b.rect->w / 2);
+	b.rect->y = y;
+
+	SDL_FreeSurface(textSurface);
+	return b;
+}
+
+void renderMenu(SDL_Renderer* renderer, TextButton* buttons, int buttonCount, int selected) {
+	SDL_RenderClear(renderer);
+
+	for (int i = 0; i < buttonCount; i++) {
+		SDL_RenderCopy(renderer, buttons[i].texture, NULL, buttons[i].rect);
+	}
+
+	// draw selection rectangle
+	TextButton selectedButton = selected == MENU_SELECTED_PLAY ? buttons[1] : buttons[2];
+
+	SDL_Rect selectionRectangle = (*selectedButton.rect);
+	selectionRectangle.x -= 16;
+	selectionRectangle.w += 30;
+
+	SDL_SetRenderDrawColor(renderer, 240, 15, 15, 80);
+
+	SDL_RenderFillRect(renderer, &selectionRectangle);
+
+	// reset color back to black
+	SDL_SetRenderDrawColor(renderer, R, G, B, 255);
+	SDL_RenderPresent(renderer);
+}
+
+void destroyMenu(TextButton** buttons, int* buttonCount) {
+	// TODO: clean up buttons
+
+	(*buttonCount) = 0;
+}
+
+TextButton* createMenuState(SDL_Renderer* renderer, TTF_Font* headerFont, TTF_Font* textFont, TextButton** buttons, int* buttonCount, int menuState) {
+	if (buttons) {
+		destroyMenu(buttons, buttonCount);
+	}
+
+	int offset = SCREEN_HEIGHT / 4;
+	SDL_Color headerColor = { 255,40,40 };
+	SDL_Color textColor = { 255,255,255 };
+	(*buttonCount) = 3;
+	switch (menuState) {
+	case MENU_STATE_WELCOME:
+		(*buttons) = (TextButton*)malloc(*buttonCount * sizeof(TextButton));
+		(*buttons)[0] = createTextButton(renderer, headerFont, headerColor, "Space Invaders", SCREEN_WIDTH / 2, offset);
+		(*buttons)[1] = createTextButton(renderer, textFont, textColor, "Play", SCREEN_WIDTH / 2, offset * 2);
+		(*buttons)[2] = createTextButton(renderer, textFont, textColor, "Quit", SCREEN_WIDTH / 2, offset * 3);
+		break;
+	case MENU_STATE_GAME_OVER:
+		(*buttons) = (TextButton*)malloc(*buttonCount * sizeof(TextButton));
+		(*buttons)[0] = createTextButton(renderer, headerFont, headerColor, "Game Over", SCREEN_WIDTH / 2, offset);
+		(*buttons)[1] = createTextButton(renderer, textFont, textColor, "Replay", SCREEN_WIDTH / 2, offset * 2);
+		(*buttons)[2] = createTextButton(renderer, textFont, textColor, "Quit", SCREEN_WIDTH / 2, offset * 3);
+		break;
+	case MENU_STATE_VICTORY:
+		(*buttons) = (TextButton*)malloc(*buttonCount * sizeof(TextButton));
+		(*buttons)[0] = createTextButton(renderer, headerFont, headerColor, "Congratulations!", SCREEN_WIDTH / 2, offset);
+		(*buttons)[1] = createTextButton(renderer, textFont, textColor, "Replay", SCREEN_WIDTH / 2, offset * 2);
+		(*buttons)[2] = createTextButton(renderer, textFont, textColor, "Quit", SCREEN_WIDTH / 2, offset * 3);
+		break;
+	}
+	return (*buttons);
+}
+
 void gameloop(SDL_Renderer* renderer) {
 	SDL_Event event;
 	const Uint8* keystate;
 	int alienDirection = ALIEN_DIRECTION_RIGHT;
-	int gameState = GAME_STATE_PLAY;
+	int gameState = GAME_STATE_MENU;
 
 	int shipCount = 1;
 	Entity* ship = createShip(renderer);
@@ -401,12 +507,24 @@ void gameloop(SDL_Renderer* renderer) {
 	int shipBulletCount = 0;
 	Object* ship_bullets = (Object*)malloc(0 * sizeof(Object));
 
-	int alienBulletCount = 0, alienBulletRelease = 10;
+	int alienBulletCount = 0, lastAlienFiringTime = 0;
 	Object* alien_bullets = (Object*)malloc(0 * sizeof(Object));
+
+	// first render takes a long-time, so call it here before game-time is considered to avoid big initial lag
+	render(renderer, ship, aliens, alienCount, ship_bullets, shipBulletCount, alien_bullets, alienBulletCount);
 
 	// variable time-step
 	int currentTicks = -1, elapsedTicks = -1, accumulatedTicks = 0, lastTicks = SDL_GetTicks();
 	int lastShipFiringTime = 0;
+
+	// menu
+	int buttonCount = -1;
+	int selected = MENU_SELECTED_PLAY;
+	bool keyDownHappened = true;
+
+	TTF_Font* textFont = TTF_OpenFont(GET_RESOURCE_PATH("Agrem.ttf"), 48);
+	TTF_Font* headerFont = TTF_OpenFont(GET_RESOURCE_PATH("Agrem.ttf"), 64);
+	TextButton* menuButtons = createMenuState(renderer, headerFont, textFont, &menuButtons, &buttonCount, MENU_STATE_WELCOME);
 
 	while (gameState != GAME_STATE_QUIT) {
 		// variable time-step
@@ -415,35 +533,51 @@ void gameloop(SDL_Renderer* renderer) {
 		accumulatedTicks += elapsedTicks;
 		lastTicks = currentTicks;
 		lastShipFiringTime += accumulatedTicks;
+		lastAlienFiringTime += accumulatedTicks;
+
+		if (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				gameState = GAME_STATE_QUIT;
+				break;
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym) {
+				case SDLK_ESCAPE:
+				case SDLK_q:
+					gameState = GAME_STATE_QUIT;
+					break;
+				}
+				keyDownHappened = true;
+				break;
+			}
+		}
 
 		if (accumulatedTicks >= TIME_STEP) {
 			switch (gameState) {
 			case GAME_STATE_PLAY:
-				if (SDL_PollEvent(&event)) {
-					switch (event.type) {
-					case SDL_QUIT:
-						gameState = GAME_STATE_QUIT;
-						break;
-					case SDL_KEYDOWN:
-						switch (event.key.keysym.sym) {
-						case SDLK_ESCAPE:
-						case SDLK_q:
-							gameState = GAME_STATE_QUIT;
-							break;
-						}
-						break;
+				keystate = SDL_GetKeyboardState(NULL);
+
+				if (ship) {
+					handleShipMovement(ship, keystate, &ship_bullets, &shipBulletCount, renderer, &lastShipFiringTime);
+				}
+				else {
+					menuButtons = createMenuState(renderer, headerFont, textFont, menuButtons, &buttonCount, MENU_STATE_GAME_OVER);
+					gameState = GAME_STATE_MENU;
+					// TODO: reinitialize game state (aliens, ship, bullets etc.)
+				}
+
+				if (alienCount > 0) {
+					moveAliens(aliens, alienCount, &alienDirection);
+
+					if (lastAlienFiringTime >= ALIEN_FIRING_DELAY) {
+						fireBullet(renderer, aliens, &alien_bullets, &alienBulletCount, true, &alienCount);
+						lastAlienFiringTime = 10;
 					}
 				}
-				keystate = SDL_GetKeyboardState(NULL);
-				if (ship)
-					handleShipMovement(ship, keystate, &ship_bullets, &shipBulletCount, renderer, &lastShipFiringTime);
-				moveAliens(aliens, alienCount, &alienDirection);
-
-				// fire alien bullet every third render
-				alienBulletRelease--;
-				if (alienBulletRelease <= 0 && alienCount > 0) {
-					fireBullet(renderer, aliens, &alien_bullets, &alienBulletCount, true, &alienCount);
-					alienBulletRelease = 10;
+				else {
+					gameState = GAME_STATE_MENU;
+					menuButtons = createMenuState(renderer, headerFont, textFont, &menuButtons, &buttonCount, MENU_STATE_VICTORY);
+					// TODO: reinitialize game state (aliens, ship, bullets etc.)
 				}
 
 				moveBullets(alien_bullets, alienBulletCount, ship_bullets, shipBulletCount);
@@ -451,12 +585,27 @@ void gameloop(SDL_Renderer* renderer) {
 				evaluateCollisions(&alien_bullets, &alienBulletCount, &ship, &(shipCount));
 
 				render(renderer, ship, aliens, alienCount, ship_bullets, shipBulletCount, alien_bullets, alienBulletCount);
-				accumulatedTicks -= TIME_STEP;
-				
 				break;
 			case GAME_STATE_MENU:
+				if (keyDownHappened) {
+					keyDownHappened = false;
+					switch (event.key.keysym.sym) {
+					case SDLK_DOWN:
+					case SDLK_UP:
+						selected = (selected + 1) % 2;
+						break;
+					case SDLK_SPACE:
+					case SDLK_KP_SPACE:
+					case SDLK_RETURN:
+					case SDLK_KP_ENTER:
+						gameState = selected == MENU_SELECTED_PLAY ? GAME_STATE_PLAY : GAME_STATE_QUIT;
+					}
+				}
+				renderMenu(renderer, menuButtons, buttonCount, selected);
 				break;
 			}
+			//accumulatedTicks -= TIME_STEP;
+			accumulatedTicks = 0;
 		}
 		else {
 			SDL_Delay(DELAY);
@@ -471,8 +620,9 @@ void gameloop(SDL_Renderer* renderer) {
 int main(int argc, char* args[])
 {
 	// Init window and PNG images loading
-	SDL_Window* window = init();
+	SDL_Window* window = initializeSdl();
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_ACCELERATED);
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
 	gameloop(renderer);
 
